435b2e120c0de6aa46d76eb1004b8fcbe455f981
Billy Campbell <wcampbell91@gmail.com>
2021-03-01 18:38:34 -0600

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './index.scss';
+import {BrowserRouter as Router } from 'react-router-dom'
+import { EconoShows } from './components/EconoShows';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <EconoShows />
+    </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function

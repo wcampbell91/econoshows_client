94a9c787aaeb9e9838fd29a915a58585a2e58405
Billy Campbell <wcampbell91@gmail.com>
2021-03-09 20:30:53 -0600

    providers are set up for use

---
+++ src/components/bands/BandProvider.js
@@ -0,0 +1,86 @@
+import React, { useState } from "react"
+
+export const BandContext = React.createContext()
+
+const BandProvider = props => {
+    const [bands, setBands] = useState([])
+    const [band, setBand] = useState({})
+
+    const getBand = (id) => {
+        return fetch(`http://localhost:8000/bands/${id}`, {
+            headers: {
+                "Authorization": `Token ${localStorage.getItem("token")}`
+            }
+        })
+        .then(res => res.json())
+        .then(setBand)
+    }
+
+    const getBands = () => {
+        return fetch("http://localhost:8000/bands", {
+            headers: {
+                "Authorization": `Token ${localStorage.getItem("token")}`
+            }
+        })
+        .then(res => res.json())
+        .then(setBands)
+        }
+
+    const registerBand = band => {
+        return fetch("http://localhost:8000/bands", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify(band)
+        })
+        .then(res => res.json())
+        // .then(res => {
+        //     if ("token" in res) {
+        //         localStorage.setItem("token", res.token)
+        //         localStorage.setItem("user_id", res.id)
+        //         props.history.push("/")
+        //     }
+        // })
+    }
+
+    const updateBand = band => {
+        return fetch(`http://localhost:8000/bands/${band.id}`, {
+            method: "PUT",
+            headers: {
+                "Authorization": `Token ${localStorage.getItem("token")}`,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(band)
+        })
+        .then(getBand)
+    }
+
+    const deleteBand = id => {
+        return fetch(`http://localhost:8000/bands/${id}`, {
+            method: "DELETE",
+            headers: {
+                "Authorization": `Token ${localStorage.getItem("token")}`
+            }
+        })
+        .then(getBands)
+    }
+
+    
+    return (
+        <BandContext.Provider value ={{
+            band,
+            bands,
+            getBand,
+            getBands,
+            registerBand,
+            updateBand,
+            deleteBand
+        }}>
+            {props.children}
+        </BandContext.Provider>
+    )
+}
+
+export default BandProvider 
---
+++ src/components/shows/ShowProvider.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react"
+
+export const ShowContext = React.createContext()
+
+const ShowProvider = props => {
+    const [ show, setShow ] = useState({})
+    const [ shows, setShows ] = useState([])
+
+    const getShows = () => {
+        return fetch("http://localhost:8000/shows")
+                    .then(res => res.json())
+                    .then(setShows)
+    }
+
+    const getShow = id => {
+        return fetch(`http://localhost:8000/shows/${id}`)
+                    .then(res => res.json())
+                    .then(setShow)
+    }
+
+    const createShow = show => {
+        return fetch("http://localhost:8000/shows", {
+            method: "POST",
+            headers: {
+                "Authorization": `Token ${localStorage.getItem("token")}`,
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify(show)
+        })
+        .then(res => res.json())
+        .then(getShows)
+    }
+
+    const updateShow = show => {
+        return fetch(`http://localhost:8000/shows/${show.id}`, {
+            method: "PUT",
+            headers: {
+                "Authorization": `Token ${localStorage.getItem("token")}`,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(show)
+        })
+        .then(getShow)
+    }
+
+    const deleteShow = id => {
+        return fetch(`http://localhost:8000/shows/${id}`, {
+            method: "DELETE"
+        })
+        .then(getShows)
+    }
+
+    return (
+        <ShowContext.Provider value={{
+            show,
+            shows,
+            getShow,
+            getShows,
+            createShow,
+            updateShow,
+            deleteShow
+        }}>
+            {props.children}
+        </ShowContext.Provider>
+    )
+}
+
+export default ShowProvider
